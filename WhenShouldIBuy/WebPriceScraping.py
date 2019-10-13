f7727bc5eaaaf9f872a62514abadf748ef3c7c20
greg008 <greg008@poczta.fm>
2019-10-14 00:00:46 +0200

    add "premium" and "mobile name" columns to csv file

--- WhenShouldIBuy/WebPriceScraping.py
+++ WhenShouldIBuy/WebPriceScraping.py
@@ -14,9 +14,20 @@ def priceScraping():
     page = urlopen(quote_page)
     soup = BeautifulSoup(page, 'html.parser')
     soup2 = str(soup)
+    # print(soup2)
+    # write data.txt
+    # html = soup.prettify("utf-8")
+    # with open("data.txt", "w") as file:
+    #     file.write(str(html))
 
     pattern = re.compile('\"statistics\"\:\{\"pageInfo\"\:\{\"lowestPrice\"\:.*nodes\"\:\[(.*)\]\}\,\"priceForecast', re.MULTILINE)
     data = re.findall(pattern, soup2)
+
+    pattern_name = re.compile('\"Product\",\"name\":\"(.*)\",\"description\":\"Price history', re.MULTILINE)
+    data_name = re.findall(pattern_name, soup2)
+    print('data_name xxxxxxxxxxxxxxx', data_name)
+    print('data name[0] = ', data_name[0])
+
     # print(type(data))
     # data = dict(data)
     print(data)
@@ -33,12 +44,24 @@ def priceScraping():
     print('split_data', split_data)
     # https://stackoverflow.com/questions/6900955/python-convert-list-to-dictionary
     d = dict(itertools.zip_longest(*[iter(split_data)] * 2, fillvalue=""))
-    print(d)
+    print('d', d)
     print(type(d))
 
     dfObj = pd.DataFrame.from_dict(d, orient='index', columns=None)
-    print('dfObj', dfObj)
+    # print('dfObj', dfObj)
+    # print('dfObj type', type(dfObj))
+    # add class type [premium, mid-range]
+    if float(dfObj.iloc[0, 0]) > 600:
+        dfObj['premium'] = 1
+    else:
+        dfObj['premium'] = 0
+    # add name of mobile phone
+    dfObj['Phone name'] = data_name[0]
 
+    # print('iloc 0 0 ', dfObj.iloc[0, 0])
+    # print('iloc 0 0  type', type(dfObj.iloc[0, 0]))
+    # dfObj['premium'] = 1
+    print('dfObj_after', dfObj)
     # data frame to csv
     index1 = ['date', 'price']
     pd.DataFrame(dfObj).to_csv('out1.csv', header=False, quoting=csv.QUOTE_NONE)
