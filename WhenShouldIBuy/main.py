05c6c3e18d70ff88108363e8b77db4ee8f42d8a4
greg008 <greg008@poczta.fm>
2019-11-05 00:53:48 +0100

    add prediction

--- WhenShouldIBuy/main.py
+++ WhenShouldIBuy/main.py
@@ -62,13 +62,47 @@ def main():
     # result [327.40021005]
     # should  be 328.3
 
+    # % percent
+
+    # podaj cene początkową np 555
+    # 'https://pricespy.co.uk/phones-gps/mobile-phones/samsung-galaxy-note-4-duos-sm-n9100-16gb--p2905869/statistics'
+
+
+    X_data_begining = 528
+    premium_no = wps.choose(X_data_begining)
+    percent_treshold = int(X_data_begining * 0.8)
+    print('X_data_begining:', X_data_begining)
+    print('percent_treshold 80%:', percent_treshold)
+
+    x_specific = pd.DataFrame()
+    x_specific.insert(0, 'Day', range(0, 364))
+    x_specific.insert(1, 'premium', premium_no)
+    y_pred_all = lin_reg_2.predict(poly_reg.fit_transform(x_specific))
+    print('y_pred_all', y_pred_all)
+
+    for i in range(y_pred_all.size):
+        if y_pred_all[i] <= percent_treshold:
+            print(i, '80% to: ', y_pred_all[i])
+            break
+    # 153 day is 80 % to: 421.96564126781067
+    # after 153 day from graph is 483 so is 10% fail and
+
+    # logic for 80%
+    # percent = 0.8 * 386
+    #
+    # for i in range(0, y_pred_specific.size):
+    #     if y_pred_specific[i] <= percent:
+    #         print('80% of begining price will be in:', i, 'day', 'and value is:', y_pred_specific[i])
+    #         break
+    # print(percent)
+
     # a= [ 0.00000000e+00 -3.32729696e-01  1.03566759e+02  1.37668444e-03
     #  -1.43230158e-01 -6.98360154e-01]
     # b= 49.57146089318843
     # RMSERMSE of polynomial regression is 1600.6850904251307
     # R2 of polynomial regression is 0.9382259298946103
     # [325.6923847]
-    
+
     # dla 400
     # a = [0.00000000e+00 - 3.39725836e-01  9.77563618e+01  1.16142115e-03
     #      - 1.28803606e-01 - 3.00586314e-01]
