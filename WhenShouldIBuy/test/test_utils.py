c6bf64da12b8197610d471ff296d1a137c0305c7
greg008 <greg008@poczta.fm>
2019-11-18 18:14:41 +0100

    add unit tests for choose func and refactor utils

---
+++ WhenShouldIBuy/test/test_utils.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append("..")
+import unittest
+
+import mock
+
+import utils as ut
+
+
+class TestUtils(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_choose_premium_price_ok(self):
+        self.assertEqual(ut.choose(456.5), 4)
+
+    def test_choose_premium_price_bounaries(self):
+        self.assertEqual(ut.choose(400), 4)
+
+    def test_choose_premium_price_fail(self):
+        self.assertNotEqual(ut.choose(456), 5)
+
+    def test_choose_premium_price_out_of_range(self):
+        self.assertNotEqual(ut.choose(45), 5)
+
+    def test_choose_premium_price_mock_else(self):
+        with mock.patch('utils.choose', return_value='Begining price is not in the correct range'):
+            self.assertEqual(ut.choose(45), 'Begining price is not in the correct range')
+
+    def test_choose_premium_price_raise_type_error(self):
+        with self.assertRaises(TypeError):
+            ut.choose('a')
+
+
+if __name__ == '__main__':
+    unittest.main()
--- WhenShouldIBuy/utils.py
+++ WhenShouldIBuy/utils.py
@@ -1,19 +1,19 @@
 
-# choose premium number from begining price
+# choose premium number dependign on begining price
 def choose(begining_price):
-    if (begining_price <= 300) and (begining_price) >= 200:
+    if (begining_price < 300) and (begining_price) >= 200:
         return 2
-    elif (begining_price <= 400) and (begining_price) > 300:
+    elif (begining_price < 400) and (begining_price) >= 300:
         return 3
-    elif (begining_price <= 500) and (begining_price) > 400:
+    elif (begining_price < 500) and (begining_price) >= 400:
         return 4
-    elif (begining_price <= 600) and (begining_price) > 500:
+    elif (begining_price < 600) and (begining_price) >= 500:
         return 5
-    elif (begining_price <= 700) and (begining_price) > 600:
+    elif (begining_price < 700) and (begining_price) >= 600:
         return 6
-    elif (begining_price <= 800) and (begining_price) > 700:
+    elif (begining_price < 800) and (begining_price) >= 700:
         return 7
-    elif (begining_price <= 900) and (begining_price) > 800:
+    elif (begining_price < 900) and (begining_price) >= 800:
         return 8
     else:
-        print('none of them')
+        print('Begining price is not in the correct range')
