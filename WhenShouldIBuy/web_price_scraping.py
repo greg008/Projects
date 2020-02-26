d3ac67a39043aaa880fe75df9ba93bd3acdd9e80
greg008 <greg008@poczta.fm>
2020-02-26 23:33:31 +0100

    remove comments

--- WhenShouldIBuy/web_price_scraping.py
+++ WhenShouldIBuy/web_price_scraping.py
@@ -38,9 +38,6 @@ def read_links():
         links = [link.strip('\n') for link in f]
     return links
 
-# req = Request('http://www.cmegroup.com/trading/products/#sortField=oi&sortAsc=false&venues=3&page=1&cleared=1&group=1', headers={'User-Agent': 'Mozilla/5.0'})
-# webpage = urlopen(req).read()
-
 def price_scraping(links):
     for link in links:
 
