cc57cb89b554f2fbd23526edd0d3b4ad93268e16
Greg <greg008@poczta.fm>
2020-07-09 15:29:33 +0200

    refactor - pep8

--- WhenShouldIBuy/web_price_scraping.py
+++ WhenShouldIBuy/web_price_scraping.py
@@ -44,7 +44,13 @@ class ScrapData:
 
     @property
     def get_data(self):
+        """get data"""
         return self.__get_data
+
+    @property
+    def url(self):
+        """url property"""
+        return self.__url
 
     @staticmethod
     def _request(url: str):
@@ -72,10 +78,6 @@ class ScrapData:
         if not any(conditions):
             raise ValueError(f"Invalid pricespy.co.uk URL:\n\n\t{self.url}")
 
-    @property
-    def url(self):
-        """url property"""
-        return self.__url
 
     @staticmethod
     def read_links():
@@ -91,92 +93,121 @@ class ScrapData:
         for arg in args:
             cols = ['date', 'price', 'premium', 'name']
             df = pd.read_csv(arg, sep=',', names=cols, header=None)
-            df = df.drop(df.index[400:])
+            df = df.drop(df.index[400:])  # first 400 rows
 
-            # convert data to integer(number of days from begining)
+            # convert data to integer(number of days from beginning)
             df.insert(0, 'Day', range(0, len(df)))
             df_concat = pd.concat([df0, df], ignore_index=True)
             df0 = df_concat
         return df0
 
     @staticmethod
-    def price_scraping(links):
+    def create_dataframe_from_dict(list_to_dict, data_name):
+        """Create pandas dataframe from dict and add additional columns"""
+        # create dataframe from dict
+        df_obj = pd.DataFrame.from_dict(list_to_dict, orient='index',
+                                        columns=None)
+        # add column with premium range
+        df_obj['premium'] = ut.deduce_premium_no(float(df_obj.iloc[3, 0]))
+
+        # add column with name of mobile phone
+        df_obj['Phone name'] = data_name[0]
+
+        return df_obj
+
+    @staticmethod
+    def create_phone_stats(df_obj):
+        """create dataframe for  each range of phones"""
+
+        dir_name = 'data'
+        filename_suffix = 'csv'
+        base_filename = 'out'
+        no_of_file = str(
+            int(ut.deduce_premium_no(float(df_obj.iloc[3, 0]))))
+
+        path = os.path.join(dir_name,
+                            no_of_file
+                            + base_filename
+                            + "."
+                            + filename_suffix)
+
+        # write dataframe to csv file
+        pd.DataFrame(df_obj).to_csv(path, header=False,
+                                    quoting=csv.QUOTE_NONE)
+
+
+    def price_scraping(self, links):
         """Extract specific statistics data from websites,
             parse and write to csv file
         """
         for link in links:
 
             # fixing mod_security problem
-            # https://stackoverflow.com/questions/16627227/http-error-403-in-python-3-web-scraping
+            # https://stackoverflow.com/questions/16627227/
+            # http-error-403-in-python-3-web-scraping
             req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
 
             page = urlopen(req)
             soup = BeautifulSoup(page, 'html.parser')
             str_soup = str(soup)
 
-            pattern = re.compile(
-                r'\"statistics\":{\"pageInfo\":{\"lowestPrice\":.*nodes'
-                r'\":\[(.*)\]\},\"priceForecast', re.MULTILINE)
-            data = re.findall(pattern, str_soup)
+            data, data_name = self.search_patterns(str_soup)
 
-            pattern_name = re.compile(r'\"Product\",\"name\":\"(.*)\",\"'
-                                      r'description\":\"Price history',
-                                      re.MULTILINE)
-            data_name = re.findall(pattern_name, str_soup)
+            list_to_dict = self.procesing_data(data)
 
-            # remove chars
-            raw_text = data[0]
-            chars = ['{', '}', '"date":', '"lowestPrice":', '"']
-
-            for c in chars:
-                raw_text = raw_text.replace(c, "")
-            split_text = raw_text.split(',')
-
-            # convert list to dictionary
-            list_to_dict = dict(
-                itertools.zip_longest(*[iter(split_text)] * 2, fillvalue=""))
-
-            # create dataframe from dict
-            df_obj = pd.DataFrame.from_dict(list_to_dict, orient='index',
-                                            columns=None)
-            df_obj['premium'] = ut.deduce_premium_no(float(df_obj.iloc[3, 0]))
-
-            # add column with name of mobile phone
-            df_obj['Phone name'] = data_name[0]
-
+            df_obj = self.create_dataframe_from_dict(list_to_dict, data_name)
             # create file name
-            dir_name = 'data'
-            filename_suffix = 'csv'
-            base_filename = 'out'
-            no_of_file = str(
-                int(ut.deduce_premium_no(float(df_obj.iloc[3, 0]))))
+            self.create_phone_stats(df_obj)
 
-            path = os.path.join(dir_name,
-                                no_of_file
-                                + base_filename
-                                + "."
-                                + filename_suffix)
+    @staticmethod
+    def procesing_data(data):
+        """Remove unnecessary text and convert to dictionary"""
+        # remove words
+        raw_text = data[0]
+        words = ['{', '}', '"date":', '"lowestPrice":', '"']
 
-            # write dataframe to csv file
-            pd.DataFrame(df_obj).to_csv(path, header=False,
-                                        quoting=csv.QUOTE_NONE)
+        for word in words:
+            raw_text = raw_text.replace(word, "")
+        split_text = raw_text.split(',')
+
+        # convert list to dictionary
+        list_to_dict = dict(
+            itertools.zip_longest(*[iter(split_text)] * 2, fillvalue=""))
+
+        return list_to_dict
+
+    @staticmethod
+    def search_patterns(str_soup):
+        """Patterns searching"""
+        pattern = re.compile(
+            r'\"statistics\":{\"pageInfo\":{\"lowestPrice\":.*nodes'
+            r'\":\[(.*)\]\},\"priceForecast', re.MULTILINE)
+        data = re.findall(pattern, str_soup)
+
+        pattern_name = re.compile(r'\"Product\",\"name\":\"(.*)\",\"'
+                                  r'description\":\"Price history',
+                                  re.MULTILINE)
+        data_name = re.findall(pattern_name, str_soup)
+
+        return data, data_name
 
     def create_final_dataset(self):
         """Create final csv datafile,
-            concat datasets from various premium number datasets
+            concat datasets from various premium number dataset
         """
+        list_of_files = ['data/2out.csv', 'data/3out.csv',
+                         'data/4out.csv', 'data/5out.csv',
+                         'data/6out.csv', 'data/7out.csv',
+                         'data/8out.csv']
 
         if self.__get_data == 'website':
-            df_concat = self.csv_to_df('data/2out.csv', 'data/3out.csv',
-                                       'data/4out.csv', 'data/5out.csv',
-                                       'data/6out.csv', 'data/7out.csv',
-                                       'data/8out.csv')
+            df_concat = self.csv_to_df(*list_of_files)
 
             # save concat df to csv
             pd.DataFrame(df_concat).to_csv('data/out_concat.csv', header=False,
                                            quoting=csv.QUOTE_NONE)
 
-            return '--------------------------csv file create from {}'.format(self.__get_data)
+            return 'csv file create from {}'.format(self.__get_data)
 
         if self.__get_data == 'database':
             postgre_sql.postgre_sql_copy_table_to_csv_file()
