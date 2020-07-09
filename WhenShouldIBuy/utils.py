37b0c32aac4834be2a7ecc9d4ed172ad5ec8407f
Greg <greg008@poczta.fm>
2020-07-09 16:54:14 +0200

    refactor

--- WhenShouldIBuy/utils.py
+++ WhenShouldIBuy/utils.py
@@ -1,19 +1,27 @@
+"""
+Helpers for web_price_scraping module
+"""
 
-# convert price to premium number depending on starting price
+
 def deduce_premium_no(starting_price):
-    if (starting_price < 300) and (starting_price) >= 200:
-        return 2
-    elif (starting_price < 400) and (starting_price) >= 300:
-        return 3
-    elif (starting_price < 500) and (starting_price) >= 400:
-        return 4
-    elif (starting_price < 600) and (starting_price) >= 500:
-        return 5
-    elif (starting_price < 700) and (starting_price) >= 600:
-        return 6
-    elif (starting_price < 800) and (starting_price) >= 700:
-        return 7
-    elif (starting_price < 900) and (starting_price) >= 800:
-        return 8
-    else:
-        print('Begining price is not in the correct range')
+    """Convert price to premium number depending on starting price"""
+    premium_no = 0
+    if 300 > starting_price >= 200:
+        premium_no = 2
+    elif 400 > starting_price >= 300:
+        premium_no = 3
+    elif 500 > starting_price >= 400:
+        premium_no = 4
+    elif 600 > starting_price >= 500:
+        premium_no = 5
+    elif 700 > starting_price >= 600:
+        premium_no = 6
+    elif 800 > starting_price >= 700:
+        premium_no = 7
+    elif 900 > starting_price >= 800:
+        premium_no = 8
+
+    if premium_no != 0:
+        return premium_no
+
+    raise TypeError('wrong range')
